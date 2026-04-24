4ed3517321c9ad6bb74e44953a918d3ca3070825
octaviog44 <octaviog2308@gmail.com>
2026-04-24 10:54:17 -0300

    Implementacion de data.ts

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  eliminado: boolean;
+  createdAt: string;
+  nombre: string;
+  precio: number;
+  descripcion: string;
+  stock: number;
+  imagen: string;
+  disponible: boolean;
+  categorias: any[]; // después lo podemos tipar mejor
+}
