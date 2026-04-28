0867b4cd687b55ebb2856971a16afbf7486341f3
octaviog44 <octaviog2308@gmail.com>
2026-04-28 11:44:59 -0300

    Añadimos un panel de administrador y fotos en los productos

--- src/pages/auth/login/login.ts
+++ src/pages/auth/login/login.ts
@@ -13,10 +13,10 @@ form.addEventListener("submit", (e: SubmitEvent) => {
   //const valuePassword = inputPassword.value;
   const valueRol = selectRol.value as Rol;
 
-  if (valueRol === "admin") {
+if (valueRol === "admin") {
   navigate("/src/pages/admin/home/home.html");
 } else if (valueRol === "client") {
-  navigate("/src/pages/client/home/home.html");
+  navigate("/src/pages/store/home/home.html");
 }
 
   const user: IUser = {
